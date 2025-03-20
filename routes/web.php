25c3058da1cf7adf21a6a57b2357853dad34958e
Alejandro Pérez Durán <apduran95@gmail.com>
2025-03-20 17:37:45 -0400

    Update welcome message and simplify route definition

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/second', function () {
+//     return view('second');
+// });
+
+Route::view(uri:'/second', view:'second'); //Simpler sintax
